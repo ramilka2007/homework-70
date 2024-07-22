ea1875a0fd6d5ec4a3d3ef71390c841ab553dd22
ramilka <lilya1976.03.03@gmail.com>
2024-07-22 22:21:21 +0600

    Создал Toolbar, где есть заголовок contacts и кнопка add new contacts. Скачал библиотеки axios, redux, bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./containers/Home/Home";
 
 const App = () => {
   return (
-    <>
-    </>
-  )
+      <>
+        <header>
+          <Toolbar />
+        </header>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+);
